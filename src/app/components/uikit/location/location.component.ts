a668af67badb733a7a780a96b163a00ab5faeed2
Perfigent <github@perfigent.com>
2023-10-27 15:44:24 +0530

    map is ready as per longitude and latitude

--- src/app/components/uikit/location/location.component.ts
+++ src/app/components/uikit/location/location.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ILocationClient, LocationModel } from 'src/app/ClientApi';
 import { ApiClient } from 'src/app/ApiClient';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-location',
@@ -13,10 +14,14 @@ export class LocationComponent implements OnInit {
   location : LocationModel[] = [];
 
   locationDialog : boolean = false;
+  
+  mapDialog : boolean = false;
 
   locationform! : FormGroup;
 
   selected! : ILocationClient;
+
+  map : any;
 
   constructor(private fb : FormBuilder, private apiClient : ApiClient) {}
 
@@ -58,5 +63,34 @@ export class LocationComponent implements OnInit {
   {
       console.log("Lattitude",lattitude)
       console.log("Longitude",longitude)
+  }
+
+  initMap(longitude: number, latitude: number): void {
+    this.mapDialog = true;
+
+    if(this.map)
+    {
+      this.map.remove();
+    }
+
+    // Delay map initialization until the dialog is open
+    setTimeout(() => {
+      this.map = L.map('map', {
+        center: [51.5, -0.09], // Use the provided latitude and longitude
+        zoom: 3
+      });
+
+      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        minZoom: 3,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      });
+
+      L.marker([51.5, -0.09]).addTo(this.map)
+      .bindPopup(`Lat : ${51.5} - Lon : ${-0.09}`)
+      .openPopup();
+
+      tiles.addTo(this.map);
+    });
   }
 }
